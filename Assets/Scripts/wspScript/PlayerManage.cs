a77f9432592a803c0083ef177273aa7247a02192
igotSmoke <1613384854@qq.com>
2024-03-23 20:00:28 +0800

    冲刺 普攻
    做了个很若只的近战攻击，上下左右控制方向，调用prefab（deAttack）为攻击框；
    更改了冲刺，现在冲刺为按L加速，最长可持续1.8s。
    增加了一个名为playerManager的对象并加上了playerManage脚本，可以用其get一些player的状态。
    一个playerAttack组件，用来控制玩家攻击的方式，武器类型等。
    deAttack可作为近战攻击的模板，新武器可以在其基础上改动。

---
+++ Assets/Scripts/wspScript/PlayerManage.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManage : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector3 getCurrentPlayerPosition()
+    {
+        GameObject hero = GameObject.Find("hero");
+        return hero.transform.position;
+    }
+
+    public Vector3 getCurrentPlayerRotation()
+    {
+        GameObject hero = GameObject.Find("hero");
+        PlayerMove PlayerMove = hero.GetComponent<PlayerMove>();
+        return PlayerMove.currentRotation;
+    }
+
+    public Vector3 getCurrentPlayerSize()
+    {
+        GameObject hero = GameObject.Find("hero");
+        BoxCollider2D collider = hero.GetComponent<BoxCollider2D>();
+        Vector3 size = Vector3.zero;
+        if (collider != null)
+        {
+            size = collider.size;
+        }
+        return size;
+    }
+}
