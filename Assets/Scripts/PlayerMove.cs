997cc5292eeb1275ba71d65579bf2fc6d9f40390
igotSmoke <1613384854@qq.com>
2024-03-23 15:51:10 +0800

    将PlayerMove完全重写
    增加了新的属性人物朝向，上下左右。
    冲刺：上左 上右 下左 下右的斜向冲刺必须在斜着移动时才可以。当原地停顿的时候按下冲刺会朝着你的当前朝向冲刺。
    deAttack 准备为最基础的默认攻击做的组件，只写了些基础函数，有待填充。
    PlayerAttack 准备调用写好的attack prefab，控制人物的攻击动作，有待填充。

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveScript : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-    public float mHeroSpeed = 13f;
-
-    private int sprintBar = 1;
-    public float coolDownSprintTime = 1.5f;
-    private float currentCoolDownSprintTime = 0f;
-    public float sprintDistanceMul = 155f;
-    private float sprintTiredTimeMul = 0.1f;
-
+    public float mHeroSpeed = 13f;//人物移动速度
+    private int sprintBar = 1;//当前可进行的冲刺次数（0||1）
+    public float coolDownSprintTime = 1.5f;//冲刺一次需要的冷却时间
+    private float currentCoolDownSprintTime = 1.5f;//当前冲刺冷却时间
+    public float sprintDistanceMul = 155f;//冲刺距离控制
+    private float sprintTiredTimeMul = 0.1f;//冲刺后不能移动的时间
+    public Vector3 currentRotation = new Vector3(1, 0, 0);//朝向,只能上下左右朝向
 
     void Start()
     {
@@ -23,28 +20,71 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        wasdMove();
-        dashMove();
+        wsadMove();
+        sprintMove();
     }
 
-    private void wasdMove()
+    private void wsadMove()
     {
-        if (Input.GetKey(KeyCode.W) && currentCoolDownSprintTime > sprintTiredTimeMul * coolDownSprintTime)
+        float verticalSpeed = Input.GetAxis("Vertical") * mHeroSpeed;
+        float horizontalSpeed = Input.GetAxis("Horizontal") * mHeroSpeed;
+
+
+        if(verticalSpeed > 0)
         {
-            transform.position += mHeroSpeed * Time.smoothDeltaTime * transform.up;
+            currentRotation.y = 1;
         }
-        if (Input.GetKey(KeyCode.S) && currentCoolDownSprintTime > sprintTiredTimeMul * coolDownSprintTime)
+        else if(verticalSpeed == 0)
         {
-            transform.position -= mHeroSpeed * Time.smoothDeltaTime * transform.up;
+            currentRotation.y = 0;
         }
-        if (Input.GetKey(KeyCode.A) && currentCoolDownSprintTime > sprintTiredTimeMul * coolDownSprintTime)
+        else if (verticalSpeed < 0)
         {
-            transform.position -= mHeroSpeed * Time.smoothDeltaTime * transform.right;
+            currentRotation.y = -1;
         }
-        if (Input.GetKey(KeyCode.D) && currentCoolDownSprintTime > sprintTiredTimeMul * coolDownSprintTime)
+
+        if (horizontalSpeed > 0)
         {
-            transform.position += mHeroSpeed * Time.smoothDeltaTime * transform.right;
+            currentRotation.x = 1;
         }
+        else if (horizontalSpeed == 0)
+        {
+            currentRotation.x = 0;
+        }
+        else if (horizontalSpeed < 0)
+        {
+            currentRotation.x = -1;
+        }
+
+        transform.position += verticalSpeed * Time.smoothDeltaTime * transform.up + horizontalSpeed * Time.smoothDeltaTime * transform.right;
+
+    }
+  
+
+    private void sprintMove()
+    {
+        if (Input.GetKeyDown(KeyCode.L) && sprintBar == 1)
+        {
+            float DisMul = 1f;
+            if (currentRotation.x != 0 && currentRotation.y != 0)
+                DisMul = 0.707f;
+            transform.position += currentRotation.y * mHeroSpeed * Time.smoothDeltaTime * transform.up * sprintDistanceMul * DisMul;
+            transform.position += currentRotation.x * mHeroSpeed * Time.smoothDeltaTime * transform.right * sprintDistanceMul * DisMul;
+            sprintBar--;
+        }
+
+        if (sprintBar == 0 && currentCoolDownSprintTime >= coolDownSprintTime)
+        {
+            currentCoolDownSprintTime -= coolDownSprintTime;
+            sprintBar++;
+        }
+
+        if (currentCoolDownSprintTime < coolDownSprintTime)
+        {
+            currentCoolDownSprintTime += Time.deltaTime;
+        }
+
+
     }
 
     private void dashMove()
