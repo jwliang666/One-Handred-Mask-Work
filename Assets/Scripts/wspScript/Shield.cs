80b24f2dcff7bd6c9c2b739b41a3b3159bf26bd6
igotSmoke <1613384854@qq.com>
2024-03-23 23:28:15 +0800

    修改了tag，给沸羊羊加了个碰撞
    修改了一些tag，给沸羊羊加了个碰撞组件，包括举盾检测和血量

--- Assets/Scripts/wspScript/Shield.cs
+++ Assets/Scripts/wspScript/Shield.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float healthCnt = 100f;
     private Rigidbody2D rb2d;
-    public bool isSpacePressed = false;
+    public bool isDefencing = false;
     private PlayerMove PlayerMove;
     private float delayTimeCnt = 0f;
     private BoxCollider2D boxCollider2D;
@@ -33,19 +32,19 @@ public class Shield : MonoBehaviour
         {
             delayTimeCnt += Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.Space) && !isSpacePressed && delayTimeCnt > 0.5f)
+        if (Input.GetKey(KeyCode.Space) && !isDefencing && delayTimeCnt > 0.5f)
         {
             rb2d.mass *= 100;
             PlayerMove.mHeroSpeed *= 0.1f;
             boxCollider2D.size *= new Vector2(2f, 2f);
-            isSpacePressed = true; // 标记空格键被按下
+            isDefencing = true; // 标记空格键被按下
         }
-        else if (!Input.GetKey(KeyCode.Space) && isSpacePressed)
+        else if (!Input.GetKey(KeyCode.Space) && isDefencing)
         {
             rb2d.mass /= 100;
             PlayerMove.mHeroSpeed *= 10f;
             boxCollider2D.size /= new Vector2(2f, 2f);
-            isSpacePressed = false; // 标记空格键松开
+            isDefencing = false; // 标记空格键松开
             delayTimeCnt = 0f;
         }
     }
