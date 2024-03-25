b97500077e4ab1f5d04af8317ac32ff0f9c7d520
zmfsk <99451613@qq.com>
2024-03-25 16:53:32 +0800

    血条

--- Assets/Scripts/wspScript/heroInjured.cs
+++ Assets/Scripts/wspScript/heroInjured.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class heroInjured : MonoBehaviour
 {
-    public int healthCnt = 200;
+    public  int healthCnt = 200;
     private Shield Shield;
     void Start()
     {
--- Assets/UIcontrol/healthbar.cs
+++ Assets/UIcontrol/healthbar.cs
@@ -6,31 +6,22 @@ using UnityEngine.UI;
 public class healthbar : MonoBehaviour
 {
     public Image bloodBar; // 血条对象
-    public float MaxHealth = 100f; // 最大生命值
-    public float CurrentHealth = 50f; // 当前生命值
-
-    void Start()
+    public float MaxHealth = 200f; // 最大生命值
+    public float CurrentHealth ; // 当前生命值
+    public  GameObject tar;
+    void Update()
     {
         UpdateHealthBar();
     }
 
     void UpdateHealthBar()
     {
+        CurrentHealth=tar.GetComponent<heroInjured>().healthCnt;
         float healthPercent = CurrentHealth / MaxHealth;
         bloodBar.fillAmount = healthPercent;
     }
 
-    public void TakeDamage(float damage)
-    {
-        CurrentHealth -= damage;
-        CurrentHealth = Mathf.Max(CurrentHealth, 0f); // 生命值不会小于0
-        UpdateHealthBar();
-    }
+   
 
-    public void Heal(float healAmount)
-    {
-        CurrentHealth += healAmount;
-        CurrentHealth = Mathf.Min(CurrentHealth, MaxHealth); // 生命值不会超过最大生命值
-        UpdateHealthBar();
-    }
+    
 }
