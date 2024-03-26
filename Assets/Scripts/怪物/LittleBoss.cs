3d8b40ec18b67c25165137fb0dc899708eaa8b75
xuy1nan <784863441@qq.com>
2024-03-26 21:38:33 +0800

    11
    1

--- Assets/Scripts/怪物/LittleBoss.cs
+++ Assets/Scripts/怪物/LittleBoss.cs
@@ -151,6 +151,7 @@ public class LittleBoss : MonoBehaviour
         else if (other.gameObject.tag == "playerBullet" /*&& (!IfLittlePlaneliving())*/)
         {
             mon3xueliang -= 1;
+            deathSound.Play();
         }
     }
 
