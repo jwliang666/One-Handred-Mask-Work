0d96c7234505bcbdd4355e447349ef19af231445
zmfsk <99451613@qq.com>
2024-03-26 20:48:08 +0800

    成功的蓝条

--- Assets/UIcontrol/healthbar.cs
+++ Assets/UIcontrol/healthbar.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class healthbar : MonoBehaviour
 {
     public Image bloodBar; // 血条对象
-    public float MaxHealth = 0.9f; // 最大生命值
+    public float MaxHealth ; // 最大生命值
     public float CurrentHealth; // 当前生命值
     public GameObject tar;
+    void Start()
+    {
+     tar = GameObject.Find("hero");
+     MaxHealth = tar.GetComponent<heroInjured>().healthCnt;
+    }
     void Update()
     {
         UpdateHealthBar();
@@ -16,8 +21,9 @@ public class healthbar : MonoBehaviour
 
     void UpdateHealthBar()
     {
-        tar = GameObject.Find("hero");
+        
         CurrentHealth = tar.GetComponent<heroInjured>().healthCnt;
+
         float healthPercent = CurrentHealth / MaxHealth;
         bloodBar.fillAmount = healthPercent;
     }
